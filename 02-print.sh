c08b5d43b1c0bc99dc88db65eaaa221e42034ab5
Adarsh <adarshhp14@gmail.com>
2021-11-01 11:57:24 +0530

    Print script additional line added for new line

--- 02-print.sh
+++ 02-print.sh
@@ -9,3 +9,11 @@
 echo Hello World
 echo Welcome
 echo Print this line
+
+# ESC Sequences , \n (new line), \t (tab space) , \e (new color)
+
+#Syntax: echo -e "Message\nNew Line"
+# To enable any esc seq we need to enable -e option
+# Also the input should be in quotes, preferably double quotes
+
+echo -e "Hello World\nWelcome"
