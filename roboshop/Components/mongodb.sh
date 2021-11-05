6d41c394104034bcebe79af0fe1392031767feb5
Adarsh <adarshhp14@gmail.com>
2021-11-05 19:59:03 +0530

    Mongodb updated

---
+++ roboshop/Components/mongodb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source components/common.sh
+
+
+Print "Download Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
+Stat $?
