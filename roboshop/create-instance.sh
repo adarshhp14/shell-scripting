e1c8d2cae834fb7da9b2574e955bf83643aea4c3
Adarsh <adarshhp14@gmail.com>
2021-11-06 14:18:36 +0530

    Create instance updated

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-CREATE() {
-  COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
-
-  if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --security-group-ids sg-0238d5e157df41b42 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
-  else
-    echo -e "\e[1;33m$1 Instance already exists\e[0m"
-  fi
-  }
+aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=webserver,Value=production}]' 'ResourceType=volume,Tags=[{Key=Name,Value=$1}]"
