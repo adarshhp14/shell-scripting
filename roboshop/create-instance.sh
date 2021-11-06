738509150ef87c947021d51c36d003634100f797
Adarsh <adarshhp14@gmail.com>
2021-11-06 15:42:01 +0530

    Create instance updated latest

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -4,7 +4,13 @@
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
 if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+  aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t2.micro --security-group-ids sg-0238d5e157df41b42--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo "Instance already exists"
 fi
+
+IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+## xargs is used to remove the double  quotes
+sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z0577679A6W027W86RBE --change-batch file:///tmp/record.json | jq
+
